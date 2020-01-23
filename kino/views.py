4ccbc11339c467e35ee4df91492c09584d75ebfe
Zeeshka <aizekdisk@gmail.com>
2020-01-23 07:38:25 +0600

    Check 404 page not found

--- kino/views.py
+++ kino/views.py
@@ -8,4 +8,11 @@ class MoviesView(View):
     # Список фильмов
     def get(self, request):
         movies = Movie.objects.all()
-        return render(request, 'movie_list.html', {'movie_list': movies})
+        return render(request, 'website/movies.html', {'movie_list': movies})
+
+
+class MovieDetailView(View):
+    # Описание фильмов
+    def get(self, request, slug):
+        movie = Movie.objects.get(url=slug)
+        return render(request, "website/movie_detail.html", {'movie': movie})
