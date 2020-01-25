be5fdcab6f4e6a2dd295c0fe3db774ea3cda2479
Zeeshka <aizekdisk@gmail.com>
2020-01-25 23:50:55 +0600

    video 9 reviews

--- kino/views.py
+++ kino/views.py
@@ -1,14 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.base import View
 from django.views.generic import ListView, DetailView
 
 from .models import Movie
+from .forms import ReviewForm
 
 
 class MoviesView(ListView):
     # Список фильмов
     model = Movie
-    queryset = Movie.objects.filter(draft=False)
+    queryset = Movie.objects.all()
     template_name = 'movies.html'
 
 
@@ -16,3 +17,15 @@ class MovieDetailView(DetailView):
     # Описание фильмов
     model = Movie
     slug_field = "url"
+    template_name = 'movie_detail.html'
+
+class AddReview(View):
+    # Отзывы
+    def post(self, request, pk):
+        form = ReviewForm(request.POST)
+        movie = Movie.objects.get(id=pk)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.movie = movie
+            form.save()
+        return redirect(movie.get_absolute_url())
